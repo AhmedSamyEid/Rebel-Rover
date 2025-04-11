e72b150af541ed9f790a9be969f918eafdd84633
Ahmed Samy <as8609377@gmail.com>
2025-04-11 13:16:21 +0200

    Add:dark-mood lit-mood

---
+++ assets/script/main.js
@@ -0,0 +1,10 @@
+document.getElementById("crescent-moon").onclick = () => {
+  if (document.body.classList.contains("crescent-moon")) {
+    document.body.classList.remove("crescent-moon");
+    document.getElementById("crescent-moon").src =
+      "assets/icons/crescent moon.webp";
+  } else {
+    document.body.classList.add("crescent-moon");
+    document.getElementById("crescent-moon").src = "assets/icons/sun.webp";
+  }
+};
