3f39ad28db021af803f3655e4f906b6f6ade269d
Ahmed Samy <as8609377@gmail.com>
2025-04-13 00:35:44 +0200

    Add : section - contact-section

--- assets/script/contact.js
+++ assets/script/contact.js
@@ -8,3 +8,35 @@ document.getElementById("crescent-moon").onclick = () => {
     document.getElementById("crescent-moon").src = "assets/icons/sun.webp";
   }
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".contact-form form");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+  
+    const name = form.querySelector('input[placeholder="Your Name"]').value;
+    const email = form.querySelector('input[placeholder="Your Email"]').value;
+    const subject = form.querySelector('input[placeholder="Subject"]').value;
+    const message = form.querySelector('textarea[placeholder="Your Message"]').value;
+
+
+    const contactData = {
+      name,
+      email,
+      subject,
+      message,
+      submittedAt: new Date().toISOString()
+    };
+
+
+    localStorage.setItem("contactFormData", JSON.stringify(contactData));
+
+
+    alert("Data saved successfully inlocalStorage ✅");
+
+
+    form.reset();
+  });
+});
